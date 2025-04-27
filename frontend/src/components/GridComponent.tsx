31db544be3335b686240043d95e8b2c47119b901
rabinzx <tonglan999@outlook.com>
2025-04-26 22:27:40 -0700

    Add gird column sorting and pager

--- frontend/src/components/GridComponent.tsx
+++ frontend/src/components/GridComponent.tsx
@@ -1,48 +1,131 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { ChevronLeftIcon, ChevronRightIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/24/solid'
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon, Bars3Icon, BarsArrowDownIcon, BarsArrowUpIcon } from '@heroicons/react/24/solid'
 
 interface GridComponentProps {
     headers: string[];
     rows: Array<Array<string | number>>;
     children?: React.ReactNode;
-    renderHeaderCell?: (header: string | number, headerIndex: number) => React.ReactNode;
+    columnSorting?: { [header: string]: boolean };
+    renderHeaderCell?: (header: string, headerIndex: number) => React.ReactNode;
     renderCell?: (cell: string | number, headerName: string, rowIndex: number, cellIndex: number,) => React.ReactNode;
 }
 
-const GridComponent: React.FC<GridComponentProps> = ({ headers, rows, children, renderHeaderCell, renderCell }) => {
-    const [pageSize, setPageSize] = useState(10); // Number of rows per page
-    const [currentPage, setCurrentPage] = useState(1);
+type SortOrder = 'asc' | 'desc' | '';
+type SortOrderDict = { [idx: number]: SortOrder };
 
-    const totalPages = useMemo(() => {
-        return Math.ceil(rows.length / pageSize)
-    }, [pageSize, rows]);
+const GridComponent: React.FC<GridComponentProps> = ({ headers, rows, children, columnSorting, renderHeaderCell, renderCell }) => {
+    // State to manage the table headers and rows
+    const [tableHeaders, setTableHeaders] = useState(headers);
+    useEffect(() => {
+        setTableHeaders(headers);
+    }, [headers]);
 
-    const currentRows = useMemo(() => {
-        return rows.slice((currentPage - 1) * pageSize, currentPage * pageSize);
-    }, [pageSize, currentPage, rows]);
-
-    const colLength = useMemo(() => {
-        return rows[0]?.length || 0;
+    const [tableRows, setTableRows] = useState(rows);
+    useEffect(() => {
+        setTableRows(rows);
     }, [rows]);
 
+    // Default page size
+    const [pageSize, setPageSize] = useState(10);
+
+    // State to manage the current page
+    const [currentPage, setCurrentPage] = useState(1);
+
+    // Calculate the total number of pages based on the number of rows and page size
+    const totalPages = useMemo(() => {
+        return Math.ceil(tableRows.length / pageSize)
+    }, [pageSize, tableRows]);
+
+    // Store the default sort order for each column
+    const sortOrderDictDefault = useMemo((): SortOrderDict => {
+        return Object.fromEntries(tableHeaders.map((_, idx) => [idx, '']))
+    }, []);
+
+    // State to manage the sort order of each column
+    const [sortOrderDict, setSortOrderDict] = useState<SortOrderDict>(sortOrderDictDefault);
+
+    // Effect to reset the current page when the table rows change
+    const currentRows = useMemo(() => {
+        return tableRows.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+    }, [pageSize, currentPage, tableRows]);
+
+    // Get the number of columns in the table
+    const columnCount = useMemo(() => {
+        return tableRows[0]?.length || 0;
+    }, [tableRows]);
+
+    // Function to navigate to a specific page
     const navigateToPage = (page: number) => {
         if (page < 1 || page > totalPages) return;
         setCurrentPage(page);
     };
 
+    // Function to handle page size changes
     const pageSizeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const newPageSize = parseInt(e.target.value, 10);
         setPageSize(newPageSize);
         setCurrentPage(1); // Reset to first page when page size changes
     }
 
+    const sortColumn = (columnIndex: number, newSortOrder: SortOrder) => {
+        let sortedRows; // Create a copy of the rows to sort
+
+        switch (newSortOrder) {
+            case 'asc':
+                sortedRows = [...tableRows].sort((a, b) => {
+                    if (a[columnIndex] < b[columnIndex]) {
+                        return -1;
+                    } else if (a[columnIndex] > b[columnIndex]) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                break;
+            case 'desc':
+                sortedRows = [...tableRows].sort((a, b) => {
+                    if (a[columnIndex] > b[columnIndex]) {
+                        return -1;
+                    } else if (a[columnIndex] < b[columnIndex]) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                break;
+            default:
+                sortedRows = [...rows]; // Reset to original order
+                break;
+        }
+
+        // Update the rows with the sorted data
+        setTableRows(sortedRows); // Uncomment this line if you want to update the original rows
+
+        // Update the sort order dictionary
+        setSortOrderDict({ ...sortOrderDictDefault, [columnIndex]: newSortOrder });
+
+    };
+
+    const displaySortIcon = (headerIndex: number) => {
+        switch (sortOrderDict[headerIndex]) {
+            case 'asc':
+                return <BarsArrowUpIcon className='size-4 inline cursor-pointer' title="Ascending" onClick={() => sortColumn(headerIndex, "desc")} />
+            case 'desc':
+                return <BarsArrowDownIcon className='size-4 inline cursor-pointer' title='Descending' onClick={() => sortColumn(headerIndex, "")} />
+            default:
+                return <Bars3Icon className='size-4 inline cursor-pointer' title='Default' onClick={() => sortColumn(headerIndex, "asc")} />
+        }
+    };
+
+
     return (
         <table className='border-collapse min-w-100 w-full shadow-md'>
             <thead>
                 <tr>
-                    {headers.map((header, headerIndex) => (
+                    {tableHeaders.map((header, headerIndex) => (
                         <th key={headerIndex} className='border p-2 bg-gray-200'>
-                            {renderHeaderCell ? renderHeaderCell(header, headerIndex) : header}
+                            {renderHeaderCell ? renderHeaderCell(header, headerIndex) : <span>{header}</span>}
+                            <span className='ml-2'>
+                                {columnSorting && columnSorting[header] && displaySortIcon(headerIndex)}
+                            </span>
                         </th>
                     ))}
                 </tr>
@@ -52,18 +135,21 @@ const GridComponent: React.FC<GridComponentProps> = ({ headers, rows, children, 
                     <tr key={rowIndex} className='bg-background hover:bg-blue-100 cursor-pointer transition-colors duration-200'>
                         {row.map((cell, cellIndex) => (
                             <td key={cellIndex} className='border p-2  text-text '>
-                                {renderCell ? renderCell(cell, headers[cellIndex], rowIndex, cellIndex) : cell}
+                                {renderCell ? renderCell(cell, tableHeaders[cellIndex], rowIndex, cellIndex) : cell}
                             </td>
                         ))}
                     </tr>
                 ))}
             </tbody>
-            <tfoot>
+            <tfoot className='text-sm'>
                 <tr>
-                    <td className='border p-2 bg-[#DEECF9]' colSpan={colLength}>
+                    <td className='border p-2 bg-[#DEECF9]' colSpan={columnCount}>
                         <div className='flex justify-end items-center gap-4'>
+                            {/* <div>
+                                {JSON.stringify(sortOrderDict)}
+                            </div> */}
                             <div>
-                                <label htmlFor="pageSize" className='mr-2'>Rows per page:</label>
+                                <label htmlFor="pageSize" className='mr-2'>rows per page:</label>
                                 <select value={pageSize} onChange={pageSizeHandler} className='border p-1 rounded'>
                                     {[10, 20, 50].map(size => (
                                         <option key={size} value={size}>{size}</option>
@@ -71,14 +157,14 @@ const GridComponent: React.FC<GridComponentProps> = ({ headers, rows, children, 
                                 </select>
                             </div>
                             <div>
-                                {(currentPage - 1) * pageSize + 1} to {Math.min(currentPage * pageSize, rows.length)} of {rows.length} items
+                                {(currentPage - 1) * pageSize + 1} to {Math.min(currentPage * pageSize, tableRows.length)} of {tableRows.length} items
                             </div>
                             <div>
-                                <ChevronDoubleLeftIcon className="size-4 inline cursor-pointer" onClick={() => navigateToPage(1)} />
-                                <ChevronLeftIcon className="size-4 inline cursor-pointer" onClick={() => navigateToPage(currentPage - 1)} />
+                                <ChevronDoubleLeftIcon className="size-4 inline cursor-pointer" title='First Page' onClick={() => navigateToPage(1)} />
+                                <ChevronLeftIcon className="size-4 inline cursor-pointer" title='Last Page' onClick={() => navigateToPage(currentPage - 1)} />
                                 <span className='px-2'>page {currentPage} of {totalPages}</span>
-                                <ChevronRightIcon className="size-4 inline cursor-pointer" onClick={() => navigateToPage(currentPage + 1)} />
-                                <ChevronDoubleRightIcon className="size-4 inline cursor-pointer" onClick={() => navigateToPage(totalPages)} />
+                                <ChevronRightIcon className="size-4 inline cursor-pointer" title='Next Page' onClick={() => navigateToPage(currentPage + 1)} />
+                                <ChevronDoubleRightIcon className="size-4 inline cursor-pointer" title='Last Page' onClick={() => navigateToPage(totalPages)} />
                             </div>
                         </div>
                     </td>
